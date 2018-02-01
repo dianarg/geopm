8dbca1f46d90d0550634f00c501baa6a3de60a9e
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2018-02-01 07:16:26 -0800

    Introducing IOGroup, a collection of signals and controls.
    - This abstraction provides a high level interface for PlatformIO
      that can be used for batch reading and writing.
    - This will also wrap MSRIO to provide the same interface while
      moving some of the code that is in PlatformIO for managing MSRs
      into the MSRIOGroup class.
    
    Change-Id: I260991e198c67faacf9f43e8ce26b2d958d13187
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

---
+++ src/TimeIOGroup.cpp
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2015, 2016, 2017, 2018, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sstream>
+
+#include "TimeIOGroup.hpp"
+#include "Exception.hpp"
+
+namespace geopm
+{
+    TimeIOGroup::TimeIOGroup()
+        : m_is_signal_pushed(false)
+    {
+        geopm_time(&m_time_zero);
+    }
+
+    TimeIOGroup::~TimeIOGroup()
+    {
+
+    }
+
+    bool TimeIOGroup::is_valid_signal(const std::string &signal_name)
+    {
+        return signal_name == "TIME";
+    }
+
+    bool TimeIOGroup::is_valid_control(const std::string &control_name)
+    {
+        return false;
+    }
+
+    int TimeIOGroup::push_signal(const std::string &signal_name, int domain_type, int domain_idx)
+    {
+        if (!is_valid_signal(signal_name)) {
+            std::ostringstream err_str;
+            err_str << "TimeIOGroup: signal_name " << signal_name << "not valid for TimeIOGroup";
+            throw Exception(err_str.str(), GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        m_is_signal_pushed = true;
+        return 0;
+    }
+
+    int TimeIOGroup::push_control(const std::string &control_name, int domain_type, int domain_idx)
+    {
+        throw Exception("TimeIOGroup::push_control() there are no controls supported by the TimeIOGroup",
+                        GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+    }
+
+    void TimeIOGroup::read_batch(void)
+    {
+        if (m_is_signal_pushed) {
+            geopm_time_s time_curr;
+            geopm_time(&time_curr);
+            m_time_curr = geopm_time_diff(&m_time_zero, &time_curr);
+        }
+    }
+
+    void TimeIOGroup::write_batch(void)
+    {
+
+    }
+
+    void TimeIOGroup::sample(std::vector<double> &signal)
+    {
+        if (m_is_signal_pushed && signal.size() > 0) {
+            signal[0] = m_time_curr;
+        }
+    }
+
+    double TimeIOGroup::sample(int batch_idx)
+    {
+        if (batch_idx) {
+            throw Exception("TimeIOGroup::sample() batch_idx out of range",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        return m_time_curr;
+    }
+
+    void TimeIOGroup::adjust(const std::vector<double> &setting)
+    {
+        if (setting.size() != 0) {
+            throw Exception("TimeIOGroup::adjust() there are no controls supported by the TimeIOGroup",
+                            GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+    }
+
+    void TimeIOGroup::adjust(int batch_idx, double setting)
+    {
+        throw Exception("TimeIOGroup::adjust() there are no controls supported by the TimeIOGroup",
+                         GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+    }
+
+    double TimeIOGroup::read_signal(const std::string &signal_name, int domain_type, int domain_idx)
+    {
+        if (!is_valid_signal(signal_name)) {
+            std::ostringstream err_str;
+            err_str << "TimeIOGroup:read_signal " << signal_name << "not valid for TimeIOGroup";
+            throw Exception(err_str.str(), GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+        }
+        geopm_time_s time_curr;
+        geopm_time(&time_curr);
+        return geopm_time_diff(&m_time_zero, &time_curr);
+    }
+
+    void TimeIOGroup::write_control(const std::string &control_name, int domain_type, int domain_idx, double setting)
+    {
+        throw Exception("TimeIOGroup::write_control() there are no controls supported by the TimeIOGroup",
+                        GEOPM_ERROR_INVALID, __FILE__, __LINE__);
+    }
+}
---
+++ src/TimeIOGroup.hpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015, 2016, 2017, 2018, Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef TIMEIOGROUP_HPP_INCLUDE
+#define TIMEIOGROUP_HPP_INCLUDE
+
+#include "IOGroup.hpp"
+#include "geopm_time.h"
+
+namespace geopm
+{
+    class TimeIOGroup : public IOGroup
+    {
+        public:
+            TimeIOGroup();
+            virtual ~TimeIOGroup();
+            virtual bool is_valid_signal(const std::string &signal_name) override;
+            virtual bool is_valid_control(const std::string &control_name) override;
+            virtual int push_signal(const std::string &signal_name, int domain_type, int domain_idx)  override;
+            virtual int push_control(const std::string &control_name, int domain_type, int domain_idx) override;
+            virtual void read_batch(void) override;
+            virtual void write_batch(void) override;
+            virtual void sample(std::vector<double> &signal) override;
+            virtual double sample(int batch_idx) override;
+            virtual void adjust(const std::vector<double> &setting) override;
+            virtual void adjust(int batch_idx, double setting) override;
+            virtual double read_signal(const std::string &signal_name, int domain_type, int domain_idx) override;
+            virtual void write_control(const std::string &control_name, int domain_type, int domain_idx, double setting) override;
+        protected:
+            bool m_is_signal_pushed;
+            geopm_time_s m_time_zero;
+            double m_time_curr;
+    };
+}
+
+#endif
