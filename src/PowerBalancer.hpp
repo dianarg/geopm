a4fb791151c87544dedec63468bbf7ba9e3d0277
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2018-07-09 09:09:40 -0700

    Clean up comments
    Change-Id: I750a721e7fff4f46b1c83d11cdb3c3c1735ab56a
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- src/PowerBalancer.hpp
+++ src/PowerBalancer.hpp
@@ -35,7 +35,7 @@
 
 namespace geopm
 {
-    /// @brief Stay within a power cap but redistibute power to
+    /// @brief Stay within a power cap but redistribute power to
     ///        optimize performance. An average per compute node power
     ///        maximum is maintained, but individual nodes will be
     ///        allowed above or below this average.
@@ -44,7 +44,7 @@ namespace geopm
         public:
             /// @brief Construct a PowerBalancer object.
             PowerBalancer() = default;
-            /// @brief Destory a PowerBalancer object.
+            /// @brief Destroy a PowerBalancer object.
             virtual ~PowerBalancer() = default;
             /// @brief Should be called at the start of application
             ///        execution with the average power cap across
@@ -63,56 +63,62 @@ namespace geopm
             /// @return The current value of the power cap.
             double power_cap(void);
             /// @brief Set the current power limit prescribed for this
-            ///        node.  Initially should be set to the power
-            ///        limit that will be maintained on average across
-            ///        the compute nodes allocated to an application.
+            ///        node.  Setting the cap with the power_cap()
+            ///        method will effectively reset the power limit
+            ///        as well to the value specified as the cap.
             /// @param limit The maximum power consumption allowed for
             ///        the compute node being managed.
             void power_limit(double limit);
             /// @brief Returns the current power limit prescribed for
             ///        this node.
+            /// @return The current power limit in units of Watts.
             double power_limit(void);
             /// @brief Update the object with a runtime measured under
             ///        the current power limit and test if the current
-            ///        runtime sample is reliable and a call
+            ///        runtime sample is reliable such that a call
             ///        runtime_sample() can be made.
             /// @param measured_runtime Most recent measurement of the
             ///        execution time for an epoch on the node being
             ///        managed under the current power limit.
+            /// @return True if a stable measurement of expected
+            ///         runtime for an epoch can be made with the
+            ///         runtime_sample() method, and false otherwise.
             bool is_runtime_stable(double measured_runtime);
             /// @brief Sample the measured runtimes under the current
-            ///        power limit.  In the first phase of execution
-            ///        this measurement will aggreated across all
+            ///        power cap in the first phase of execution.
+            ///        This measurement will aggregated across all
             ///        compute nodes to find the largest runtime
-            ///        measured.  In the second phase of execution,
-            ///        the value will be passed to next_limit() in
-            ///        order to test the execution time at a lower
-            ///        limit.
+            ///        measured.
             /// @return The expected execution time of an application
             ///         epoch under the current power limit.
             double runtime_sample(void);
             /// @param Set the target runtime which is the largest
-            ///        epoch execution time measured since the
-            ///        application began or the last global increase
-            ///        to the power budget.
+            ///        epoch execution time measured by any compute
+            ///        node since the application began or the last
+            ///        global increase to the power budget.
             /// @param largest_runtime The largest expected runtime
             ///        for one epoch across all compute nodes under
             ///        the current power budget.
             void target_runtime(double largest_runtime);
             /// @brief During the second phase of execution the power
-            ///        limit is increased until the epoch runtime on
-            ///        the compute node under managment has increased
+            ///        limit is decreased until the epoch runtime on
+            ///        the compute node under management has increased
             ///        to the runtime of the slowest compute node.
             ///        This method is used to update the object with a
             ///        new measurement and also test if the current
             ///        power limit meets the requirements.  If the
             ///        method returns false, then the value returned
-            ///        by power_limit() may be modified.  The new
+            ///        by power_limit() may have been updated.  The new
             ///        power limit should enforced for the next epoch
             ///        execution.
             /// @param measured_runtime Most recent measurement of the
             ///        execution time for an epoch on the node being
             ///        managed under the current power limit.
+            /// @return True if the current power limit is reliably
+            ///         close to the target runtime and excess power
+            ///         should be sent up to the root to be
+            ///         redistributed, and false if more trials are
+            ///         required.
             bool is_target_met(double measured_runtime);
         private:
             enum m_step_e {
@@ -122,7 +128,7 @@ namespace geopm
                 /// @brief Aggregate epoch runtime up tree by applying
                 ///        maximum filter to measured values.
                 M_STEP_SEND_UP_RUNTIME,
-                /// @brief Propegate down from root the longest
+                /// @brief Propagate down from root the longest
                 ///        recorded runtime from any node.
                 M_STEP_SEND_DOWN_RUNTIME,
                 /// @brief Decrease power limit on all nodes (other
@@ -150,8 +156,12 @@ namespace geopm
             bool m_is_excess_ready;
             int m_step;
             int m_iteration;
-            double m_power_cap; // Maximum power as set in last global budget increase.
-            double m_power_limit; // Current power limit to get to target runtime which may be lower than the cap.
+            // @brief Maximum power as set in last global budget
+            //        increase.
+            double m_power_cap;
+            // @brief Current power limit to get to target runtime
+            //        which may be lower than the cap.
+            double m_power_limit;
             double m_target_runtime;
             CircularBuffer<double> m_runtime_buffer;
     };
