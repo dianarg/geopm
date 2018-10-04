04929e878535df0f9a1054630ee96967a41ffb54
Diana Guttman <diana.r.guttman@intel.com>
2018-10-04 13:23:49 -0700

    Add SKX to supported platforms for energy efficient tests.
    Change-Id: I1b10ca265c11c45e467dd153189b6616996cbd7a
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- test_integration/geopm_test_integration.py
+++ test_integration/geopm_test_integration.py
@@ -77,10 +77,10 @@ def get_platform():
     return fam, mod
 
 
-def skip_unless_platform_bdx():
+def skip_unless_platform_bdx_skx():
     fam, mod = get_platform()
-    if fam != 6 or mod not in (45, 47, 79):
-        return unittest.skip("Performance test is tuned for BDX server, The family {}, model {} is not supported.".format(fam, mod))
+    if fam != 6 or mod not in (45, 47, 79, 85):
+        return unittest.skip("Performance test is tuned for BDX or SKX server, The family {}, model {} is not supported.".format(fam, mod))
     return lambda func: func
 
 
@@ -1018,7 +1018,7 @@ class TestIntegration(unittest.TestCase):
             self.assertLessEqual(1, len(gemm_region))
 
     @skip_unless_run_long_tests()
-    @skip_unless_platform_bdx()
+    @skip_unless_platform_bdx_skx()
     @skip_unless_cpufreq()
     def test_agent_energy_efficient_offline(self):
         """
@@ -1036,6 +1036,9 @@ class TestIntegration(unittest.TestCase):
         if hostname.endswith('.alcf.anl.gov'):
             dgemm_bigo = dgemm_bigo_jlse
             stream_bigo = stream_bigo_jlse
+        elif hostname.startswith('mcfly'):
+            dgemm_bigo = 42.0
+            stream_bigo = 1.7
         else:
             dgemm_bigo = dgemm_bigo_quartz
             stream_bigo = stream_bigo_quartz
@@ -1064,7 +1067,7 @@ class TestIntegration(unittest.TestCase):
 
         # Setup the static policy run
         num_node = 1
-        num_rank = 4
+        num_rank = 1
         # Runs frequency sweep, generates best-fit frequency mapping, and
         # runs with the plugin in offline mode.
         analysis = geopmpy.analysis.OfflineBaselineComparisonAnalysis(profile_prefix=name,
@@ -1096,7 +1099,7 @@ class TestIntegration(unittest.TestCase):
         self.assertLess(-10.0, runtime_savings_epoch)  # want -10% or better
 
     @skip_unless_run_long_tests()
-    @skip_unless_platform_bdx()
+    @skip_unless_platform_bdx_skx()
     @skip_unless_cpufreq()
     def test_agent_energy_efficient_online(self):
         """
