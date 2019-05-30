76e99aa48cc39034ccfb8e2023ca031ca682adae
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2019-05-30 11:29:47 -0700

    Fixup report filter script
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- test_integration/test_ee_freq_sweep_report_filter.sh
+++ test_integration/test_ee_freq_sweep_report_filter.sh
@@ -4,4 +4,4 @@ grep '^Region stream' -A 10 test_ee_freq_sweep.report | \
 egrep '^Region stream|requested' | \
 sed 's|.*\(stream-[0-9\.]*-dgemm-[0-9\.]*\).*| \1|g' | \
 sed 's|    requested-online-frequency: |:|' | \
-tr -d '\n' | tr ' ' '\n' | grep . | sort
+tr -d '\n' | tr ' ' '\n' | grep . | sed 's|stream-||' | sort -n | sed 's|^|stream-|'
