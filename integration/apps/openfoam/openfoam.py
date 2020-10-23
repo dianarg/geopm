9d11e9a9912435d8bd765c32dc638f7c38607c06
Diana Guttman <diana.r.guttman@intel.com>
2020-10-23 16:13:53 -0700

    Add baseline run script for openfoam
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- integration/apps/openfoam/openfoam.py
+++ integration/apps/openfoam/openfoam.py
@@ -42,15 +42,7 @@ class OpenfoamAppConf(apps.AppConf):
     def __init__(self, num_nodes, mach, pin_config):
         self._benchmark_dir = os.path.dirname(os.path.abspath(__file__))
 
-        app_cores = mach.num_core()
-        if pin_config == 'all_cores':
-            pass
-        elif pin_config == 'geopm_os_shared':
-            app_cores -= 1
-        elif pin_config == 'geopm_os_reserved':
-            app_cores -= 2
-        else:
-            raise RuntimeError("Unknown pin_config: {}".format(pin_config))
+        app_cores = apps.get_available_app_cores(mach, pin_config)
 
         self._num_nodes = num_nodes
         self._ranks_per_node = app_cores
