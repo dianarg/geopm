d0b20d2aa4414764ad3c59a3883ff702013fe26e
Diana Guttman <diana.r.guttman@intel.com>
2020-11-15 14:46:37 -0800

    Add gen smoke test results to db
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- integration/smoke/gen_smoke.sh
+++ integration/smoke/gen_smoke.sh
@@ -43,9 +43,12 @@ fi
 GEOPM_SOURCE=${GEOPM_SOURCE:?Please set GEOPM_SOURCE in your environment.}
 source ${GEOPM_SOURCE}/integration/config/run_env.sh
 EXP_DIR=${GEOPM_SOURCE}/integration/experiment
+RESULT_DB_SCRIPT=${GEOPM_SOURCE}/integration/smoke/db_demo/smoke.py
+
+# TODO: fix; needed for mysql dependency
+source ${GEOPM_SOURCE}/integration/smoke/db_demo/venv/bin/activate
 
 APPLICATIONS="dgemm dgemm_tiny nekbone minife amg nasft hpcg hpl_mkl hpl_netlib pennant"
-
 
 function check {
    if [ $? -ne 0 ]; then
@@ -57,10 +60,13 @@ function check {
 function print_result {
     if [ $result -eq 0 ]; then
         echo -e "\e[1;32m[ PASS ] $EXP_TYPE with $APP\e[0m" 1>&2
+        ${RESULT_DB_SCRIPT} --gen-result --app=${APP} --exp-type=${EXP_TYPE} --result="PASS"
     elif [ $result -eq 1 ]; then
         echo -e "\e[1;31m[ FAIL ] $EXP_TYPE with $APP\e[0m" 1>&2
+        ${RESULT_DB_SCRIPT} --gen-result --app=${APP} --exp-type=${EXP_TYPE} --result="FAIL"
     elif [ $result -eq 2 ]; then
         echo -e "\e[1;33m[ SKIP ] $EXP_TYPE with $APP\e[0m" 1>&2
+        ${RESULT_DB_SCRIPT} --gen-result --app=${APP} --exp-type=${EXP_TYPE} --result="N/A"
     fi
 }
 
