320b2dce07c6ac732c850be5716612a5e330714b
Diana Guttman <diana.r.guttman@intel.com>
2020-11-16 09:47:35 -0800

    WIP adding more apps and exp types
    - TODO: many empty jobs being spawned just to write 'noscript' as a result.  launching script can do this check instead.
    
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- integration/smoke/db_demo/gen_all.sh
+++ integration/smoke/db_demo/gen_all.sh
@@ -3,13 +3,14 @@
 #ALL_APPS="dgemm_tiny amg dgemm minife nekbone hpcg nasft"
 #ALL_EXP="monitor power_sweep frequency_sweep power_balancer_energy barrier_frequency_sweep"
 
-APPS_2NODE="dgemm_tiny nasft nekbone"
+APPS_2NODE="dgemm_tiny dgemm nasft nekbone hpcg pennant hpl_mkl hpl_netlib"
 # apps with no 2-node config
 APPS_1NODE="minife amg"
-ALL_EXP="monitor power_sweep"
+ALL_EXP="monitor power_sweep power_balancer_energy frequency_sweep barrier_frequency_sweep uncore_frequency_sweep"
 for exp in $ALL_EXP; do
     for app in $APPS_2NODE; do
         ./gen_sbatch.py --app=${app} --exp-type=${exp} --node-count=2
+        # TODO: check if run script exists, skip and directly log run result here
         sbatch ${app}_${exp}.sbatch
     done
 
