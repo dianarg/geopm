a3054446040216e1a94cb6496150aab1928ef725
Diana Guttman <diana.r.guttman@intel.com>
2020-12-17 17:52:52 -0800

    Extract common environment setup into smoke_env.sh
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

--- integration/smoke/gen_smoke.sh
+++ integration/smoke/gen_smoke.sh
@@ -31,18 +31,8 @@
 #  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-# TODO: fix this
-if [ -f ${HOME}/.geopmrc ]; then
-    source ${HOME}/.geopmrc
-fi
+source smoke_env.sh
 
-if [ ! -z ${GEOPM_SYSTEM_ENV} ]; then
-    source ${GEOPM_SYSTEM_ENV}
-fi
-
-GEOPM_SOURCE=${GEOPM_SOURCE:?Please set GEOPM_SOURCE in your environment.}
-source ${GEOPM_SOURCE}/integration/config/run_env.sh
-EXP_DIR=${GEOPM_SOURCE}/integration/experiment
 RESULT_DB_SCRIPT=${GEOPM_SOURCE}/integration/smoke/db_demo/smoke.py
 
 # TODO: fix; needed for mysql dependency
