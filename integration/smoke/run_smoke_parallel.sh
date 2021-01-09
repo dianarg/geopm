a77b247552040c16c2b3a5ea602c26aa3133a7a2
Diana Guttman <diana.r.guttman@intel.com>
2021-01-09 10:20:41 -0800

    Add script to run smoke tests in parallel jobs
    Signed-off-by: Diana Guttman <diana.r.guttman@intel.com>

---
+++ integration/smoke/run_smoke_parallel.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source smoke_env.sh
+
+#ALL_APPS="dgemm_tiny amg dgemm minife nekbone hpcg nasft"
+#ALL_EXP="monitor power_sweep frequency_sweep power_balancer_energy barrier_frequency_sweep"
+
+APPS_2NODE="dgemm_tiny dgemm nasft nekbone hpcg pennant hpl_mkl hpl_netlib"
+# apps with no 2-node config
+APPS_1NODE="minife amg"
+ALL_EXP="monitor power_sweep power_balancer_energy frequency_sweep barrier_frequency_sweep uncore_frequency_sweep"
+
+EXP_DIR=${EXP_ROOT}/monitor
+for exp in $ALL_EXP; do
+    for app in ${APPS_2NODE}; do
+        SCRIPT=${EXP_DIR}/run_${exp}_${app}.py
+        if [ -f $SCRIPT ]; then
+            ./gen_sbatch.py --app=${app} --exp-type=${exp} --node-count=2
+            sbatch ${app}_${exp}.sbatch
+        else
+            echo "$SCRIPT not found"
+            ${GEOPM_SOURCE}/integration/smoke/db_demo/smoke.py --app=${app} --exp-type=${exp} --result="No script"
+        fi
+    done
+
+    for app in ${APPS_1NODE}; do
+        SCRIPT=${EXP_DIR}/run_${exp}_${app}.py
+        if [ -f $SCRIPT ]; then
+            ./gen_sbatch.py --app=${app} --exp-type=${exp} --node-count=1
+            sbatch ${app}_${exp}.sbatch
+        else
+            echo "$SCRIPT not found"
+            ${GEOPM_SOURCE}/integration/smoke/db_demo/smoke.py --app=${app} --exp-type=${exp} --result="No script"
+        fi
+    done
+
+done
